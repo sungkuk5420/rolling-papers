3199cf9206d4c988b3e3dba73824cd403205c9de
sungkuk5420 <sungkuk5420@gmail.com>
2023-02-27 17:21:07 +0900

    만들기 누르고 로그인안한경우 생성하기 세션 제거하기

--- src/ComputedMixin.js
+++ src/ComputedMixin.js
@@ -6,9 +6,18 @@ let ComputedMixin = {
             nickname: "getNickname",
             uid: "getUid",
             themeGroupList: "getThemeGroupList",
+            loginGuideLayer: "getLoginGuideLayer",
         }),
     },
     watch: {
+        loginGuideLayer(value) {
+            if (!value) {
+                localStorage.removeItem('groupUid');
+                localStorage.removeItem('groupName');
+                localStorage.removeItem('groupCode');
+                localStorage.removeItem('selectTheme');
+            }
+        }
     }
 };
 export default ComputedMixin;
